423ae7ba5b52926207883438a3304657e9281859
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2022-08-16 01:13:31 +0300

    Add project files.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success) // iki parametre gönderilirse bu hem alttaki tek parametreli versiyonu hem de iki parametreli versiyonu çalıştırır
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        public string? Message { get; }
+    }
+}
