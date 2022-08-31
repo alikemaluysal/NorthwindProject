a8537221f0eb0faaf54a15b4ce2b3cba49a93db3
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2022-08-31 03:49:55 +0300

    JWT, Cache, Transaction and Performance Implemented

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,5 +27,14 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Aynı isimde ürün eklenemez!";
 
         public static string CategoryLimitExceded = "Kategori limiti aşıldığından sisteme eklenemiyor";
+
+        public static string AuthorizationDenied = "Yetkiniz yok!";
+
+        public static string UserRegistered = "Kullanıcı başarıyla kayıt oldu!";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string PasswordError = "Şifre hatası!";
+        public static string SuccessfulLogin = "Başarıyla giriş yapıldı!";
+        public static string UserAlreadyExists = "Kullanıcı zaten sistemde kayıtlı!";
+        public static string AccessTokenCreated = "Access Token oluşturuldu!";
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,18 @@
 ﻿using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI
 {
@@ -25,11 +33,36 @@ namespace WebAPI
 
             builder.Services.AddControllers();
 
+            var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
+
+            builder.Services.AddDependencyResolvers(
+                new CoreModule()
+                );
+
             #region Old Dependency Injection
-            builder.Services.AddSingleton<IProductService, ProductManager>(); // Arka planda bir referans oluştur içerisinde data tutmuyorsa singleton kullan, tüm projede 1 tane nesne oluşuyor
-            builder.Services.AddSingleton<IProductDal, EfProductDal>();
+            //builder.Services.AddSingleton<IProductService, ProductManager>(); // Arka planda bir referans oluştur içerisinde data tutmuyorsa singleton kullan, tüm projede 1 tane nesne oluşuyor
+            //builder.Services.AddSingleton<IProductDal, EfProductDal>();
             #endregion
 
+            #region Value cannot be null Error Fix (Old)
+            //builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //ServiceTool.Create(builder.Services);
+            #endregion
 
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -48,6 +81,8 @@ namespace WebAPI
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
 
             app.MapControllers();
